ef091622ad3fd0ee18da223faea084f9786e7684
Tigistyitayew <chinayitayew@gmail.com>
2023-06-30 05:48:59 -0700

    network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -s -X PUT -d "user_id=98" -H "Origin: You got me!" -L 0.0.0.0:5000/catch_me
+# Displays 'You got me!' when makes request to 0.0.0.0:5000/catch_me
+curl -X PUT -L -H "Origin: HolbertonSchool" -s 0.0.0.0:5000/catch_me
+-d 'user_id=98'
